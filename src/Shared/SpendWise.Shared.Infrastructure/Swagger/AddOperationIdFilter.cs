6dac310eecf1e90a9cd532c10b10201346e1b16a
Kamil Kurdziel <kurdzielson@gmail.com>
2023-12-03 15:55:28 +0100

    Modify OperationId Filter

--- src/Shared/SpendWise.Shared.Infrastructure/Swagger/AddOperationIdFilter.cs
+++ src/Shared/SpendWise.Shared.Infrastructure/Swagger/AddOperationIdFilter.cs
@@ -4,21 +4,20 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace SpendWise.Shared.Infrastructure.Swagger;
 
-internal class AddOperationIdFilter : IOperationFilter
+internal abstract class AddOperationIdFilter : IOperationFilter
 {
     private static readonly Regex MyRegex = new Regex("(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z])", RegexOptions.Compiled);
 
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        if (operation.OperationId == null)
-        {
-            var controllerName = context.MethodInfo.DeclaringType?.Name;
-            operation.OperationId = GetOperationId(controllerName);
-        }
+        if (operation.OperationId != null) return;
+
+        var controllerName = context.MethodInfo.DeclaringType?.Name;
+        var actionName = context.MethodInfo.Name;
+
+        operation.OperationId = GetOperationId(controllerName, actionName);
     }
 
-    private static string GetOperationId(string className)
-    {
-        return MyRegex.Replace(className, "_$1").Trim();
-    }
+    private static string GetOperationId(string className, string actionName)
+        => MyRegex.Replace(className, "_$1").Trim() + "_" + MyRegex.Replace(actionName, "_$1").Trim();
 }
